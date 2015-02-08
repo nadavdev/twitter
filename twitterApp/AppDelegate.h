dc121dcd3ebe2591faebe7fc4ccf25d1156ad818
nadavg <nadavg@yahoo-inc.com>
2015-02-07 21:02:51 -0800

    Initial Commit

---
+++ twitterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitterApp
+//
+//  Created by Nadav Golbandi on 2/7/15.
+//  Copyright (c) 2015 Nadav Golbandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
